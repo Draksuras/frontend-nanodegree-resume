717df7a70d7025b37024e2711ad6c55fc0eff75b
Paulo Moreira <moreira.a.paulo@gmail.com>
2015-05-06 21:41:29 -0500

    Added bio, education, work, and projects JSON objects.

---
+++ js/resumeBuilder.js
@@ -0,0 +1,76 @@
+
+var bio = {
+
+    "name" : "Paulo Moreira",
+    "role" : "Front-End Web Developer",
+    "contacts" : {
+        "mobile": "N/A",
+        "email": "moreira.a.paulo@gmail.com",
+        "github": "https://github.com/Draksuras",
+        "location": "Winnipeg"
+        },
+    "welcomeMessage": "Welcome to my Resume",
+    "skills": ["TO", "fill", "out", "later"],
+    "biopic": "https:/notdoneyet",
+    //"display": function() {return 0;}
+    
+
+}
+
+var education = {
+
+    "schools": [
+        {
+
+            "name": "University of Manitoba",
+            "location": "Winnipeg",
+            "degree": "Science",
+            "majors": ["Chemistry"],
+            "dates": 2011,
+            "url": "http://umanitoba.ca"
+        }
+    ],
+
+    "onlineCourses": [
+        {
+            "title": "Introduction to Programming",
+            "school": "Rice university",
+            "date": 2014,
+            "url": "edx.org"
+    }]
+}
+
+var work = {
+
+    "jobs": [
+        {
+            "employer": "New Flyer",
+            "title": "Customer Support Analyst",
+            "location": "Winnipeg",
+            "dates": "Aug-2014 - PResent",
+            "description": "Analyzing data to gain insights in improving customer fleet's operations."
+        },
+        {
+            "employer": "Sequoia Energy",
+            "title": "Resource Assessmennt Tech.",
+            "location": "Winnipeg",
+            "dates": "2011-2014",
+            "description": "Analyzed data for wind power site suitability."
+        }
+
+    ]
+}
+
+var projects = {
+
+    "projects": [
+        {
+
+            "title": "Asteroids Game",
+            "dates": "2014",
+            "description": "Asteroids game implemented in Python",
+            "images": "http://needstobefilledout"
+        }
+
+    ]
+}
